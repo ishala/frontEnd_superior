55a9c37d7a1a44ad8dff056664f0a535f0778818
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2024-04-05 04:35:53 +0700

    add img_bg

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,12 @@
+// import Footer from '../component/Footer';
+import backgroundImage from '../assets/img/main_bg.jpg';
+
 function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="max-w-md w-full py-10 px-6 bg-white shadow-md rounded-md">
-        <h2 className="text-2xl font-semibold mb-4">Welcome to DioPark</h2>
-        <p className="text-gray-700">Sabar ya belom jadi WOIII.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className="max-w-md w-full py-10 px-6 bg-white shadow-md rounded-md" style={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+        <h2 className="text-2xl text-center font-semibold mb-4">Welcome to DioPark</h2>
+        <p className="text-gray-700 text-center">Sabar ya belom jadi WOIII.</p>
       </div>
     </div>
   );
