5cf60b4d10430ea4853b902c01fadb1c4a72dbaa
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2024-04-05 04:53:05 +0700

    create page NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import Register from './page/Register';
 import Login from './page/Login';
 import Home from './page/Home';
+import NotFound from './page/NotFound';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -47,6 +48,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<AuthRoute element={<Register onRegister={handleRegister} />} />} />
         <Route path="/login" element={<AuthRoute element={<Login onLogin={handleLogin} />} />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
