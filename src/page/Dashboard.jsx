cbe05981b39691be3a4bc5f4235741965df2d292
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2024-04-05 08:00:53 +0700

    making alert for lepi

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -10,6 +10,7 @@ function Dashboard() {
     const [scanning, setScanning] = useState(false);
     const [cameraImage, setCameraImage] = useState(null);
     const [isBackCamera, setIsBackCamera] = useState(true);
+    const [showAlert, setShowAlert] = useState(false);
     const videoRef = useRef(null);
 
     useEffect(() => {
@@ -68,15 +69,33 @@ function Dashboard() {
         setIsBackCamera(!isBackCamera);
     };
 
+    const handleScanButtonClick = () => {
+        if (isMobile()) {
+            setScanning(true);
+        } else {
+            setShowAlert(true);
+        }
+    };
+
+    const closeAlert = () => {
+        setShowAlert(false);
+    };
+
+    // Function to detect if the user is on a mobile device
+    const isMobile = () => {
+        const userAgent = navigator.userAgent;
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
+    };
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-            <div className="max-w-md w-full py-10 px-6 bg-white bg-opacity-50 shadow-md rounded-md">
+            <div className="max-w-md w-full py-10 px-6 bg-white bg-opacity-50 shadow-md rounded-md lg:max-w-lg lg:mx-auto" style={{ borderRadius: '20px', maxWidth: 'calc(100vw - 40px)' }}>
                 <h2 className="text-2xl text-center font-semibold mb-4">
                     Parkir lebih mudah dengan DioPark, Tinggal scan aja dibawah ini
                 </h2>
                 {!scanning && !cameraImage && (
                     <div className="flex flex-col items-center justify-center space-y-4">
-                        <button onClick={() => setScanning(true)} className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">
+                        <button onClick={handleScanButtonClick} className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">
                             <FontAwesomeIcon icon={faQrcode} className="mr-2" />Scan tiket
                         </button>
                         <label htmlFor="file-upload" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
@@ -87,7 +106,7 @@ function Dashboard() {
                 )}
                 {(scanning || cameraImage) && (
                     <div className="relative">
-                        <video ref={videoRef} style={{ width: '100%', borderRadius: '10px' }} />
+                        <video ref={videoRef} className="w-full rounded-md" />
                         <button onClick={() => { setScanning(false); setCameraImage(null); }} className="absolute top-2 right-2 text-red-500">
                             <FontAwesomeIcon icon={faTimes} size="2x" />
                         </button>
@@ -106,6 +125,17 @@ function Dashboard() {
                     </div>
                 )}
             </div>
+            {showAlert && (
+                <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
+                    <div className="bg-white p-6 rounded-md shadow-lg">
+                        <p className="text-lg text-center">Silahkan akses menggunakan handphone</p>
+                        <div className='flex items-center justify-center'>
+                            <button onClick={closeAlert} className="bg-gray-500 text-white px-4 py-2 mt-4 rounded-md hover:bg-gray-600">Tutup</button>
+                        </div>
+
+                    </div>
+                </div>
+            )}
             <Footer />
         </div>
     );
