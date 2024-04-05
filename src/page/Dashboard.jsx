6ae1005a790a10c0713e8b1038ae207a8ae6df88
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2024-04-05 07:21:26 +0700

    make it responsive

--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -69,17 +69,17 @@ function Dashboard() {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-            <div className="max-w-md w-full py-10 px-6 bg-white bg-opacity-50 shadow-md rounded-md" style={{ borderRadius: '20px' }}>
+        <div className="flex flex-col items-center justify-center min-h-screen" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+            <div className="max-w-md w-full py-10 px-6 bg-white bg-opacity-50 shadow-md rounded-md">
                 <h2 className="text-2xl text-center font-semibold mb-4">
                     Parkir lebih mudah dengan DioPark, Tinggal scan aja dibawah ini
                 </h2>
                 {!scanning && !cameraImage && (
-                    <div className="flex items-center justify-center">
-                        <button onClick={() => setScanning(true)} className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 inline-block mr-2">
+                    <div className="flex flex-col items-center justify-center space-y-4">
+                        <button onClick={() => setScanning(true)} className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">
                             <FontAwesomeIcon icon={faQrcode} className="mr-2" />Scan tiket
                         </button>
-                        <label htmlFor="file-upload" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 inline-block">
+                        <label htmlFor="file-upload" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
                             <FontAwesomeIcon icon={faUpload} className="mr-2" />Upload Qr
                         </label>
                         <input id="file-upload" type="file" accept="image/*" onChange={handleImageUpload} className="hidden" />
